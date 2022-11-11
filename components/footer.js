ab8e2b4d724336b425e4c1b762eb81c2af62acb4
Ashok Kumar <ap271449@gmail.com>
2022-11-11 09:57:05 +0530

    responsive footer

--- components/footer.js
+++ components/footer.js
@@ -11,62 +11,63 @@ let blueSection = () => {
 }
 
 let footer = () => {
-  return ` <div id="buffer">
-  <h1>Buffer</h1>
-  <div id="links">
-    <div id="s-links">
-      <i class="fa-brands fa-facebook-f"></i>
-      <i class="fa-brands fa-instagram"></i>
-      <i class="fa-brands fa-twitter"></i>
-      <i class="fa-brands fa-tiktok"></i>
-      <i class="fa-brands fa-linkedin-in"></i>
-      <i class="fa-brands fa-pinterest-p"></i>
-    </div>
-    <div id="download">
-      <p>Download</p>
-      <button class="d-btn">
-        <i class="fa-brands fa-apple"></i> App Store
-      </button>
-      <button class="d-btn">
-        <i class="fa-brands fa-google-play"></i> Play Store
-      </button>
-    </div>
-    <div id="copyright">
-      Copyright ©2022 Buffer | Privacy | Terms | Security
+  return ` 
+  <div id="buffer">
+   <h1>MiddleMan</h1>
+    <div id="links">
+      <div id="s-links">
+        <i class="fa-brands fa-facebook-f"></i>
+        <i class="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-tiktok"></i>
+        <i class="fa-brands fa-linkedin-in"></i>
+        <i class="fa-brands fa-pinterest-p"></i>
+      </div>
+      <div id="download">
+        <p>Download</p>
+        <button class="d-btn">
+          <i class="fa-brands fa-apple"></i> App Store
+        </button>
+        <button class="d-btn">
+          <i class="fa-brands fa-google-play"></i> Play Store
+        </button>
+      </div>
+      <div id="copyright">
+        Copyright ©2022 Buffer | Privacy | Terms | Security
+      </div>
     </div>
   </div>
-</div>
-<div id="tools" class="lh">
-  <h4>Tools</h4>
-  <p>Publishing</p>
-  <p>Analytics</p>
-  <p>Engagement</p>
-  <p>Start Page</p>
-  <p>Extras</p>
-</div>
-<div id="resources" class="lh">
-  <h4>Resources</h4>
-  <p>Blog</p>
-  <p>Content library</p>
-  <p>Brower Extension</p>
-  <p>Free Image Editor</p>
-</div>
-<div id="support" class="lh">
-  <h4>Support</h4>
-  <p>Help Center</p>
-  <p>Status</p>
-  <p>Changelog</p>
-  <p>Product Roadmap</p>
-</div>
-<div id="company" class="lh">
-  <h4>Company</h4>
-  <p>About</p>
-  <p>Transparency</p>
-  <p>Carrers</p>
-  <p>Accesibility</p>
-  <p>Press</p>
-  <p>Sitemap</p>
-</div>
+  <div id="tools" class="lh">
+    <h4>Tools</h4>
+    <p>Publishing</p>
+    <p>Analytics</p>
+    <p>Engagement</p>
+    <p>Start Page</p>
+    <p>Extras</p>
+  </div>
+  <div id="resources" class="lh">
+    <h4>Resources</h4>
+    <p>Blog</p>
+    <p>Content library</p>
+    <p>Brower Extension</p>
+    <p>Free Image Editor</p>
+  </div>
+  <div id="support" class="lh">
+    <h4>Support</h4>
+    <p>Help Center</p>
+    <p>Status</p>
+    <p>Changelog</p>
+    <p>Product Roadmap</p>
+  </div>
+  <div id="company" class="lh">
+    <h4>Company</h4>
+    <p>About</p>
+    <p>Transparency</p>
+    <p>Carrers</p>
+    <p>Accesibility</p>
+    <p>Press</p>
+    <p>Sitemap</p>
+  </div>
 </div>`
 }
 
