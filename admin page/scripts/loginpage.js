32f93eb49936fe5e428045c1becd2d9fc0225946
Ashok Kumar <ap271449@gmail.com>
2022-11-12 21:58:49 +0530

    linked : admin page

--- admin page/scripts/loginpage.js
+++ admin page/scripts/loginpage.js
@@ -1,12 +1,12 @@
-let login_btn=document.getElementById("login_btn")
- login_btn.onclick=()=>{
- let username=document.getElementById("username").value
- let password=document.getElementById("password").value    
-let user_detials={
- username,
- password
-}
-localStorage.setItem("userdata",JSON.stringify(user_detials))
- 
-window.location.replace("./landing_page.html")
+let login_btn = document.getElementById("login_btn")
+login_btn.onclick = () => {
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+    let user_detials = {
+        username,
+        password
+    }
+    localStorage.setItem("userdata", JSON.stringify(user_detials))
+
+    window.location.replace("./landing_page.html")
 }
